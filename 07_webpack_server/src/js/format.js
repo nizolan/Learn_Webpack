2bdf45cd9d57f4e5798a32a854fdd3a39257b8db
nizolan <nizolan@163.com>
2022-04-28 18:10:08 +0800

    add

---
+++ 07_webpack_server/src/js/format.js
@@ -0,0 +1,8 @@
+const priceFormat = function () {
+  return "¥100.10";
+}
+
+// CommonJS的导出
+module.exports = {
+  priceFormat
+}
---
+++ 07_webpack_server/src/vue/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<h2>{{ title }}</h2>
+	<p>{{ content }}</p>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "我是APP标题",
+				content: "我是APP内容哈哈哈哈",
+			};
+		},
+	};
+</script>
+
+<style scoped>
+	h2 {
+		color: red;
+	}
+	p {
+		color: blue;
+	}
+</style>
+
